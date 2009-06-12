85743cf56394b4cc8cc839ade28c5fc53264ebca
Wolfgang Kinkeldei <wolfgang@macbook.(none)>
2009-06-12 19:41:40 +0200

    created a hierarchy view

--- root/bycode/hierarchy/index.pl
+++ root/bycode/hierarchy/index.pl
@@ -6,15 +6,15 @@ sub hierarchy_level {
     my $hierarchy = shift;
     return if (!$hierarchy || ref($hierarchy) ne 'ARRAY' || !scalar(@{$hierarchy}));
     
-    my $level = $hierarchy->[0]->{level};
+    my $level = $hierarchy->[0]->get_column('level');
     ul {
         while (scalar(@{$hierarchy})) {
             my $h = $hierarchy->[0];
-            if ($h->{level} == $level) {
+            if ($h->get_column('level') == $level) {
                 # same level -> simply draw
                 li {$h->name};
                 shift @{$hierarchy};
-            } elsif ($h->{level} > $level) {
+            } elsif ($h->get_column('level') > $level) {
                 hierarchy_level($hierarchy);
             } else {
                 return;
@@ -25,6 +25,9 @@ sub hierarchy_level {
 
 sub RUN {
     h1 {"hierarchy"};
+    
+    print OUT "ref(h) = " . ref(stash->{hierarchy}->[0]);
+    br;
     
     with {id => 'hierarchy'} div {
         hierarchy_level( [ @{stash->{hierarchy}} ] );
